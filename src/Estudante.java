419220d73806f131c71b559f406375f1c5005fb0
Paulo <paulosergio.games1997@gmail.com>
2024-10-09 10:22:08 -0300

    exercicio0001

---
+++ src/Estudante.java
@@ -0,0 +1,44 @@
+public class Estudante {
+    String nome;
+    int matricula;
+    double nota1;
+    double nota2;
+    double nota3;
+
+    // O construtor deve declarar que pode lançar a exceção
+    public Estudante(String nome, int matricula, double nota1, double nota2, double nota3) throws NotaInvalidaException {
+        this.nome = nome;
+        this.matricula = matricula;
+        setNotas(nota1, nota2, nota3);  // Pode lançar NotaInvalidaException
+    }
+
+    public double getMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    // Define as notas, lançando exceção se alguma for inválida
+    public void setNotas(double nota1, double nota2, double nota3) throws NotaInvalidaException {
+        if (nota1 < 0 || nota2 < 0 || nota3 < 0) {
+            throw new NotaInvalidaException("As notas não podem ser negativas.");
+        }
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    double getNota1() {
+        return nota1;
+    }
+
+    double getNota2() {
+        return nota2;
+    }
+
+    double getNota3() {
+        return nota3;
+    }
+
+    public boolean isAprovado() {
+        return getMedia() >= 7;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Criação do estudante, que pode lançar NotaInvalidaException
+            Estudante estudante = new Estudante("Paulo", 5, 7.3, 7.8, 7.0);
+
+            // Imprime as informações do estudante
+            System.out.println("Nome: " + estudante.nome);
+            System.out.println("Matricula: " + estudante.matricula);
+            System.out.println("1º Nota: " + estudante.getNota1());
+            System.out.println("2º Nota: " + estudante.getNota2());
+            System.out.println("3º Nota: " + estudante.getNota3());
+            System.out.printf("Média final: %.1f%n", estudante.getMedia());
+
+            // Verifica se o estudante foi aprovado
+            if (estudante.isAprovado()) {
+                System.out.printf("O aluno %s foi aprovado.%n", estudante.nome);
+            } else {
+                System.out.printf("O aluno %s foi reprovado.%n", estudante.nome);
+            }
+
+        } catch (NotaInvalidaException e) {
+            // Trata a exceção lançada caso uma nota inválida seja passada
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
