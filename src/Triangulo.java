55bc78bbb9bbd375804d81c469b3e6445c09f210
Paulo <paulosergio.games1997@gmail.com>
2024-10-11 22:05:19 -0300

    ...

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,3 +1,6 @@
+//biblioteca para digitar os valores dos lados pelo teclado.
+import java.util.Scanner;
+
 public class Triangulo {
     private double ladoA;
     private double ladoB;
@@ -25,12 +28,16 @@ public class Triangulo {
     }
 
     // Metodo que ira determinar o tipo do triangulo.
-    public String i(double ladoA, double ladoB, double ladoC) {
+    public String i() {
         if (getLadoA() == getLadoB() && getLadoA() == getLadoC())
             return "EQUILATERO";
-        else if (getLadoA() == getLadoB() || getLadoA() == getLadoC() || getLadoB() == getLadoC())
+        else if (getLadoA() == getLadoB() ||
+                getLadoA() == getLadoC() ||
+                getLadoB() == getLadoC())
             return "ISOSCELES";
-        else if (getLadoA() != getLadoB() && getLadoA() != getLadoC() && getLadoB() != getLadoC())
+        else if (getLadoA() != getLadoB() &&
+                getLadoA() != getLadoC() &&
+                getLadoB() != getLadoC())
             return "ESCALENO";
         else
             return "NENHUMA OPCAO";
@@ -38,11 +45,31 @@ public class Triangulo {
 
     // Metodo para determinar se um triangulo e valido
     public Boolean ii() {
-        // VOU FAZER QUANDO VOLTAR
+        return getLadoA() + getLadoB() > getLadoC() &&
+               getLadoA() + getLadoC() > getLadoB() &&
+               getLadoB() + getLadoC() > getLadoA();
     }
 
     // metodo principal.
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        Triangulo triangulo = new Triangulo();
+
+        System.out.print("Digite o tamanho do lado A: ");
+        double a = input.nextDouble();
+        System.out.print("Digite o tamanho do lado B: ");
+        double b = input.nextDouble();
+        System.out.print("Digite o tamanho do lado C: ");
+        double c = input.nextDouble();
+
+        triangulo.setLados(a, b, c);
+
+        if (triangulo.ii())
+            System.out.printf("O triangulo de lados %.1f x %.1f x %.1f eh valido e ele eh %s\n", triangulo.getLadoA(),
+            triangulo.getLadoB(), triangulo.getLadoC(), triangulo.i());
+        else
+            System.out.printf("O valores %.1f x %.1f x %.1f não tornam o triangulo valido.", triangulo.getLadoA(),
+            triangulo.getLadoB(), triangulo.getLadoC());
     }
 }
